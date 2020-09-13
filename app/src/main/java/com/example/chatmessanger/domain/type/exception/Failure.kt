75217d07439cef485dc19a5402e8181bbc0c913f
jeka1488 <mogudi88>
2020-09-13 16:29:02 +0300

    Create Domain

---
+++ app/src/main/java/com/example/chatmessanger/domain/type/exception/Failure.kt
@@ -0,0 +1,11 @@
+package com.example.chatmessanger.domain.type.exception
+
+
+/**
+ * Base Class for handling errors/failures/exceptions.
+ */
+
+sealed class Failure {
+    object NetworkConnectionError : Failure()
+    object ServerError : Failure()
+}
