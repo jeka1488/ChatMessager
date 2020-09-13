cc92efd4814b999b3b684705453cead94ad9730b
jeka1488 <mogudi88>
2020-09-13 17:30:17 +0300

    Create Remote segment

--- app/src/main/java/com/example/chatmessanger/cache/AccountCacheImpl.kt
+++ app/src/main/java/com/example/chatmessanger/cache/AccountCacheImpl.kt
@@ -1,4 +1,18 @@
 package com.example.chatmessanger.cache
 
-class AccountCacheImpl {
+import com.example.chatmessanger.data.account.AccountCache
+import com.example.chatmessanger.domain.type.Either
+import com.example.chatmessanger.domain.type.None
+import com.example.chatmessanger.domain.type.exception.Failure
+import javax.inject.Inject
+
+class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefsManager) : AccountCache {
+
+    override fun saveToken(token: String): Either<Failure, None> {
+        return prefsManager.saveToken(token)
+    }
+
+    override fun getToken(): Either<Failure, String> {
+        return prefsManager.getToken()
+    }
 }
