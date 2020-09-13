c1e39f3a41d355b4ce8bb72600a7dcdf304904c8
jeka1488 <mogudi88>
2020-09-13 16:51:18 +0300

    Create Remote segment

--- app/src/main/java/com/example/chatmessanger/remote/core/NetworkHendler.kt
+++ app/src/main/java/com/example/chatmessanger/remote/core/NetworkHendler.kt
@@ -1,4 +1,18 @@
 package com.example.chatmessanger.remote.core
 
-class NetworkHendler {
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Injectable class which returns information about the network connection state.
+ */
+@Singleton
+class NetworkHandler @Inject constructor(private val context: Context) {
+    val isConnected get() = context.networkInfo?.isConnected
 }
+
+val Context.networkInfo: NetworkInfo? get() =
+    (this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
